51fc46c81ea2d1b7c08bb0ab40243099686c408a
Nick T <nicksforums@gmail.com>
2022-08-29 00:15:27 +0700

    new update

--- front-react-client/src/components/Dashboard.js
+++ front-react-client/src/components/Dashboard.js
@@ -4,10 +4,23 @@ import "bootstrap/dist/css/bootstrap.min.css"
  class Dashboard extends Component {
   render() {
     return  (
-      <div>
-      <h1 className='alert alert-warning'>welcome to Dashboard</h1>
-      <ProjectItem /> 
+      <div className="projects">
+      <div className="container">
+          <div className="row">
+              <div className="col-md-12">
+                  <h1 className="display-4 text-center">Projects</h1>
+                  <br />
+                  <a href="ProjectForm.html" className="btn btn-lg btn-info">
+                      Create a Project
+                  </a>
+                  <br />
+                  <hr />
+
+             
+              </div>
+          </div>
       </div>
+  </div>
     );
 
     
