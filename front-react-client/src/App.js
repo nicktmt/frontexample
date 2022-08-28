589b0ef8a810898b279b65cf513ae90f1eb76998
Nick T <nicksforums@gmail.com>
2022-08-29 00:16:11 +0700

    update

--- front-react-client/src/App.js
+++ front-react-client/src/App.js
@@ -1,17 +1,20 @@
 import './App.css';
 import Dashboard from './components/Dashboard';
 import Header from './components/Layout/Header';
-import "bootstrap/dist/css/bootstrap.min.css"
+import "bootstrap/dist/css/bootstrap.min.css";
+import {BroswerRouter as Router, Route} from "react-router-dom";
 
 function App() {
   return (
+    <Router>
+
     <div className="App">
-    
       <Header />
       <Dashboard />
 
 
     </div>
+    </Router>
   );
 }
 
