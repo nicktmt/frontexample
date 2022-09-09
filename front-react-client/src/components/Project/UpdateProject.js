cb4b96979e19f07729184b08ae11116773bc3958
Nick T <nicksforums@gmail.com>
2022-09-09 17:06:41 +0700

    new

---
+++ front-react-client/src/components/Project/UpdateProject.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class UpdateProject extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Update project</h1>
+      </div>
+    )
+  }
+}
