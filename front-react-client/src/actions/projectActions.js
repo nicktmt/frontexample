744254a435c44e0b22a5ebf74fd281b2a111a73e
Nick T <nicksforums@gmail.com>
2022-09-15 16:12:41 +0700

    new

--- front-react-client/src/actions/projectActions.js
+++ front-react-client/src/actions/projectActions.js
@@ -1,5 +1,5 @@
 import axios from "axios";
-import { GET_ERRORS, GET_PROJECT, GET_PROJECTS } from "./types";
+import { GET_ERRORS, GET_PROJECT, GET_PROJECTS, DELETE_PROJECT } from "./types";
 import { toast } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
 import {useParams} from 'react-router-dom';
@@ -23,9 +23,13 @@ export const createProject = (project) => async dispatch =>{
 
     try{
         const resp = await axios.post("http://localhost:8080/api/project",project)
-        window.location.assign('/dashboard')
+        window.location.assign('/dashboard');
+        dispatch({
+            type: GET_ERRORS,
+            payload: {}
+        });
+        
     }catch(err) {
-        console.log("Loi day nay");
           dispatch({
             type: GET_ERRORS,
             payload: err.response.data
@@ -43,10 +47,27 @@ export const createProject = (project) => async dispatch =>{
     };
 
     export const getProject = (id) => async dispatch => {
+        try {
+            dispatch({
+                type: GET_ERRORS,
+                payload: {}
+            });
+            const res = await axios.get (`http://localhost:8080/api/project/${id}`)
+            dispatch({
+                type: GET_PROJECT,
+                payload: res.data
+            });
+        } catch (error) {
+            window.location.assign('/dashboard');
+            
+        }
+       
+    };
 
-        const res = await axios.get (`http://localhost:8080/api/project/${id}`)
+    export const deleteProject = (id) => async dispatch => {
+        await axios.delete(`http://localhost:8080/api/project/${id}`)
         dispatch({
-            type: GET_PROJECT,
-            payload: res.data
+            type: DELETE_PROJECT,
+            payload: id
         });
     };
